f1d36902b505327fff3d7ca9794b38b52b8a28c6
yahya <yahya@DESKTOP-17F6FSU>
2021-03-20 22:37:42 +0100

    Version 1

--- src/main/java/net/ausiasmarch/gamingstore/proyectofinal/api/ClienteController.java
+++ src/main/java/net/ausiasmarch/gamingstore/proyectofinal/api/ClienteController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package api;
+package net.ausiasmarch.gamingstore.proyectofinal.api;
 
 import java.util.List;
 import java.util.Optional;

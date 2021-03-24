4695e3af2e61f57ef814f7376db9a04e98ae8812
yahya <yahya@DESKTOP-17F6FSU>
2021-03-24 13:39:31 +0100

    Version 1

--- src/test/java/net/ausiasmarch/gamingstore/proyectofinal/ProyectofinalApplicationTests.java
+++ src/test/java/net/ausiasmarch/gamingstore/proyectofinal/ProyectofinalApplicationTests.java
@@ -1,5 +1,5 @@
 package net.ausiasmarch.gamingstore.proyectofinal;
-
+/*
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,3 +11,4 @@ class ProyectofinalApplicationTests {
 	}
 
 }
+*/
